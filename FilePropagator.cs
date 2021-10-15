70dca846e80e467b58673d937f6072f8b007433b
thepicture <fuzzyeastern@protonmail.com>
2021-10-15 14:02:24 +0100

    Add project files.

---
+++ FilePropagator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirectoryFilePropagationUtilsLibrary
+{
+    public class FilePropagator
+    {
+        public static void Propagate(string rootFolderPath, string fileSource)
+        {
+
+        }
+    }
+}
