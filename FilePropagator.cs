5b82396fa9ab10dcfed751e9a114ebe57d8bd3e1
thepicture <fuzzyeastern@protonmail.com>
2021-10-15 14:26:42 +0100

    Wrote unit tests from FilePropagator

--- FilePropagator.cs
+++ FilePropagator.cs
@@ -1,16 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.Win32;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows;
 
 namespace DirectoryFilePropagationUtilsLibrary
 {
     public class FilePropagator
     {
-        public static void Propagate(string rootFolderPath, string fileSource)
+        /// <summary>
+        /// Propagates the given file to every subfolder and to the root folder.
+        /// </summary>
+        /// <param name="rootFolderPath">The root folder path.</param>
+        /// <param name="sourceFileName">The file path.</param>
+        /// <returns>Boolean which indicates if the propagating ran correctly.</returns>
+        public static bool Propagate(string rootFolderPath, OpenFileDialog fileDialog)
         {
+            try
+            {
+                File.Copy(
+                    fileDialog.FileName,
+                    Path.Combine(
+                            rootFolderPath,
+                            fileDialog.SafeFileName
+                    )
+                );
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
 
+            string[] directories = Directory
+                .GetDirectories(rootFolderPath);
+
+            if (directories.Count() == 0)
+            {
+                return true;
+            }
+
+            foreach (var childDirectory in directories)
+            {
+                Propagate(childDirectory, fileDialog);
+            }
+
+            return true;
         }
     }
 }
